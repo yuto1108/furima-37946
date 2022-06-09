238fe2adb552a24d15eb04c710dab930cbf1e5a9
yuto1108 <yuto-1108@au.com>
2022-06-09 16:02:13 +0900

    S3導入

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -12,7 +12,7 @@ const pay = () => {
       exp_month: formData.get("purchase_order[exp_month]"),
       exp_year: `20${formData.get("purchase_order[exp_year]")}`,
     };
-    
+
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
